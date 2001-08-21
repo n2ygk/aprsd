e129e8d457572fa95da29b8b84c5c92c95b6762c
Hamish Moffatt <hmoffatt@users.sourceforge.net>
2001-08-21 12:41:57 +0000

    Some changes for compilation with g++ 3.0.

--- src/except.hpp
+++ src/except.hpp
@@ -1,17 +1,17 @@
 #include <exception>
 #include <string>
 
-
 class TAprsdException: public exception {
 
 private:
     string what_str;
 
 public:
-    TAprsdException(const string &what_arg):
+    TAprsdException(const string &what_arg) throw ():
         what_str(what_arg) { }
-    virtual const char *what() const {
+    virtual const char *what() const throw () {
         return what_str.c_str();
     }
+    ~TAprsdException() throw () { }; // Required by compiler
 };
 
