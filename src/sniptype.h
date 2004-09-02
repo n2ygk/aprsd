467adc6d357f973b761636706d6db613d080cdae
Chuck Byam <kg4ijb@users.sourceforge.net>
2004-09-02 12:57:22 +0000

    Added missing header

---
+++ src/sniptype.h
@@ -0,0 +1,56 @@
+/*
+ * $Id$
+ *
+ * aprsd, Automatic Packet Reporting System Daemon
+ * Copyright (C) 1997,2002 Dale A. Heatherington, WA4DSY
+ * Copyright (C) 2001-2002 aprsd Dev Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Look at the README for more information on the program.
+ */
+
+
+#ifndef SNIPTYPE__H
+#define SNIPTYPE__H
+
+#include <stdlib.h>                             /* For free()           */
+#include <string.h>                             /* For NULL & strlen()  */
+
+typedef enum {Error_ = -1, Success_, False_ = 0, True_} Boolean_T;
+
+#if !defined(WIN32) && !defined(_WIN32) && !defined(__NT__) \
+      && !defined(_WINDOWS)
+ #if !defined(OS2)
+  typedef unsigned char  BYTE;
+  typedef unsigned long  DWORD;
+ #endif
+ typedef unsigned short WORD;
+#else
+ #define WIN32_LEAN_AND_MEAN
+ #define NOGDI
+ #define NOSERVICE
+ #undef INC_OLE1
+ #undef INC_OLE2
+ #include <windows.h>
+ #define HUGE
+#endif
+
+#define NUL '\0'
+#define LAST_CHAR(s) (((char *)s)[strlen(s) - 1])
+#define TOBOOL(x) (!(!(x)))
+#define FREE(p) (free(p),(p)=NULL)
+
+#endif /* SNIPTYPE__H */
